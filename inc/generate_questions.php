02276feb4c5ea6b472c65d06ecf053ffb795a228
Teslim Adeyemo <teslimni@gmail.com>
2019-11-05 11:56:22 +0100

    Ensured that the random answers are unique

--- inc/generate_questions.php
+++ inc/generate_questions.php
@@ -1,9 +1,11 @@
 <?php
 // Loop for required number of questions
 $quizElements = [];
+
 // Get random numbers to add
 $leftAdder = rand(10, 100);
 $rightAdder = rand(30, 100);
+
 // Generate random questions
 $randQuestion = "What is {$leftAdder}  + {$rightAdder}";
 
@@ -11,14 +13,23 @@ $randQuestion = "What is {$leftAdder}  + {$rightAdder}";
 $answer = $leftAdder + $rightAdder;
 
 // Get incorrect answers within 10 numbers either way of correct answer
-$firstIncorrectAnswer = rand(3, 10);
-$secondIncorrectAnswer = rand(4, 10);
+$firstIncorrectAnswer = rand(-10, 10);
+$secondIncorrectAnswer = rand(-10, 10);
 
-// Make sure it is a unique answer
+/**
+ * Make sure it is a unique answer
+ * This makes sure that each of the answer option is unique and that no answer is predictable.
+ */
+if ($firstIncorrectAnswer == $secondIncorrectAnswer || $answer == $firstIncorrectAnswer || $answer == $secondIncorrectAnswer || $firstIncorrectAnswer == 0 || $firstIncorrectAnswer == -$firstIncorrectAnswer || $secondIncorrectAnswer == 0 || $secondIncorrectAnswer == -$secondIncorrectAnswer) {
+    $firstIncorrectAnswer += 2;
+    $secondIncorrectAnswer += 5;
+} 
+
 $answers[] = $answer;
 $answers[] = $answer + $firstIncorrectAnswer;
-$answers[] = $answer - $secondIncorrectAnswer;
+$answers[] = $answer + $secondIncorrectAnswer;
 
+// Shuffle the answers so that the correct answer shows upi different position each time
 shuffle($answers);
 
 // Build the quiz elements
